58d9575a4aa0e4efb6fe90502032ea6e7f26e994
NOT_A_ROBOT <not_a_normal_robot@protonmail.com>
2024-06-13 11:31:00 +0700

    Make faq entries mostly styled

--- components/faq/faq-entry.vue
+++ components/faq/faq-entry.vue
@@ -8,6 +8,7 @@ const props = defineProps({
     }
 });
 </script>
+
 <template>
     <details
       :id="entry.id"
@@ -15,7 +16,7 @@ const props = defineProps({
       name="faq">
         <summary>
             <h3>{{ $t(`faq.entries.${entry.id}.title`) }}</h3>
-            <span>
+            <span class="tags">
                 {{ $t("faq.preTags")}}
                 <FaqTag
                     v-for="tag in entry.tags"
@@ -27,3 +28,53 @@ const props = defineProps({
         <cite>{{ entry.author }}</cite>
     </details>
 </template>
+
+<style scoped lang="scss">
+details {
+    border: 1px solid lime;
+    border-radius: 0.4rem;
+    transition: 200ms;
+    margin-bottom: 1rem;
+
+    font-size: 1.25rem;
+    background-color: rgba(0,100,0,0.15);
+    color: white;
+
+    > summary {
+        height: min-content;
+        padding: 0.5rem 1rem;
+        background-color: rgba(0,100,0,0.5);
+        border: 0 none transparent;
+        color: lime;
+        cursor: pointer;
+        font-size: 1.25rem;
+        flex-grow: 1;
+        text-align: left;
+
+        h3 {
+            display: inline;
+        }
+
+        span.tags {
+            display: flex;
+            flex-direction: row;
+            height: fit-content;
+            align-items: center;
+            justify-items: start;
+            margin-top: 0.5em;
+            gap: 0.5em;
+            font-size: 0.9em;
+        }
+
+        &::marker {
+            display: inline-block;
+            text-align: center;
+            margin-right: 1rem;
+            width: 2rem; height: 2rem;
+            align-self: center;
+            border: 1px solid lime;
+            border-radius: 0.2rem;
+        }
+    }
+}
+</style>
