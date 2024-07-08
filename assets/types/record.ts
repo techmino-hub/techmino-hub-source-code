4f6ba5aa51ca19a39ff70de72932ca0640c0fa32
Not-A-Normal-Robot <not_a_normal_robot@protonmail.com>
2024-07-08 11:55:06 +0700

    work on schema

--- assets/types/record.ts
+++ assets/types/record.ts
@@ -41,44 +41,170 @@ export type RecordSchema = {
     order: string,
 }
 
+const survivorSchema: RecordSchema = {
+    entries: {
+        0: {
+            name: 'wave',
+            type: 'number'
+        },
+        1: {
+            name: 'time',
+            type: 'number'
+        },
+        date: {
+            name: 'date',
+            type: 'string'
+        }
+    },
+    
+    order: 'score->>0 DESC, score->>1 ASC, score->>date ASC'
+};
+
+const backfireSchema: RecordSchema = {
+    entries: {
+        0: {
+            name: 'attack',
+            type: 'number'
+        },
+        1: {
+            name: 'time',
+            type: 'number'
+        },
+        date: {
+            name: 'date',
+            type: 'string'
+        }
+    },
+    
+    order: 'score->>0 DESC, score->>1 ASC, score->>date ASC'
+};
+
+const c4wSchema: RecordSchema = {
+    entries: {
+        0: {
+            name: 'combo',
+            type: 'number'
+        },
+        1: {
+            name: 'time',
+            type: 'number'
+        },
+        date: {
+            name: 'date',
+            type: 'string'
+        }
+    },
+
+    order: 'score->>0 DESC, score->>1 ASC, score->>date ASC'
+};
+
+const classicSchema: RecordSchema = {
+    entries: {
+        0: {
+            name: 'score',
+            type: 'number'
+        },
+        1: {
+            name: 'lines',
+            type: 'number'
+        },
+        date: {
+            name: 'date',
+            type: 'string'
+        }
+    },
+
+    order: 'score->>0 DESC, score->>1 DESC, score->>date ASC'
+};
+
+const constructSchema: RecordSchema = {
+    entries: {
+        0: {
+            name: 'height',
+            type: 'number'
+        },
+        1: {
+            name: 'pieces',
+            type: 'number'
+        },
+        date: {
+            name: 'date',
+            type: 'string'
+        }
+    },
+
+    order: 'score->>0 DESC, score->>1 ASC, score->>date ASC'
+};
+
+const marathonSchema: RecordSchema = {
+    entries: {
+        0: {
+            name: 'lines',
+            type: 'number'
+        },
+        1: {
+            name: 'time',
+            type: 'number'
+        },
+        date: {
+            name: 'date',
+            type: 'string'
+        }
+    },
+
+    order: 'score->>0 DESC, score->>1 ASC, score->>date ASC'
+};
+
+const sprintSchema: RecordSchema = {
+    entries: {
+        0: {
+            name: 'lines',
+            type: 'number'
+        },
+        1: {
+            name: 'time',
+            type: 'number'
+        },
+        date: {
+            name: 'date',
+            type: 'string'
+        }
+    },
+    
+    // better lines, then better time, then earlier date
+    order: 'score->>0 DESC, score->>1 ASC, score->>date ASC'
+};
 
 export const RECORD_SCHEMAS = {
-    attacker_h: {
-        entries: {
-            0: {
-                name: 'wave',
-                type: 'number'
-            },
-            1: {
-                name: 'time',
-                type: 'number'
-            },
-            date: {
-                name: 'date',
-                type: 'string'
-            }
-        },
-        
-        order: 'score->>0 DESC, score->>1 ASC, score->>date ASC'
-    },
-    attacker_u: {
-        entries: {
-            0: {
-                name: 'wave',
-                type: 'number'
-            },
-            1: {
-                name: 'time',
-                type: 'number'
-            },
-            date: {
-                name: 'date',
-                type: 'string'
-            }
-        },
-
-        order: 'score->>0 DESC, score->>1 ASC, score->>date ASC'
-    }
+    attacker_h: survivorSchema,
+    attacker_u: survivorSchema,
+    backfire_n: backfireSchema,
+    backfire_h: backfireSchema,
+    backfire_l: backfireSchema,
+    backfire_u: backfireSchema,
+    big_n: marathonSchema,
+    big_h: marathonSchema,
+    blind_e: marathonSchema,
+    blind_n: marathonSchema,
+    blind_h: marathonSchema,
+    blind_l: marathonSchema,
+    blind_u: marathonSchema,
+    blind_wtf: marathonSchema,
+    c4wtrain_n: c4wSchema,
+    c4wtrain_l: c4wSchema,
+    classic_e: classicSchema,
+    classic_n: classicSchema,
+    classic_h: classicSchema,
+    classic_l: classicSchema,
+    classic_u: classicSchema,
+    classic_fast: classicSchema,
+    construct_sg: constructSchema,
+    construct_checker: constructSchema,
+    construct_invsg: constructSchema,
+    defender_n: survivorSchema,
+    defender_l: survivorSchema,
 } as {
     [modeID: string]: RecordSchema
-}
+};
+
+export default RECORD_SCHEMAS;
