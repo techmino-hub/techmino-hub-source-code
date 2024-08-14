c113e2b6ef7ed8e94782a04d359b3160fabfa9ba
Not-A-Normal-Robot <not_a_normal_robot@protonmail.com>
2024-08-14 11:29:41 +0700

    Update parser to support 0.17.22+

--- assets/scripts/replay/parser.ts
+++ assets/scripts/replay/parser.ts
@@ -13,16 +13,20 @@ function decodeVLQ(data: Uint8Array, position: number): [number, number] {
     return [ret, position];
 }
 
-function pumpRecording(data: Uint8Array): GameInputEvent[] {
+function pumpRecording(data: Uint8Array, absoluteTiming = false): GameInputEvent[] {
     let position = 0;
     const events = [] as GameInputEvent[];
 
     let curFrame = 0;
     while (position < data.length) {
-        let dt: number, eventKey: number;
+        let time: number, eventKey: number;
         
-        [dt, position] = decodeVLQ(data, position);
-        curFrame += dt;
+        [time, position] = decodeVLQ(data, position);
+        if(absoluteTiming) {
+            curFrame = time;
+        } else {
+            curFrame += time;
+        }
 
         [eventKey, position] = decodeVLQ(data, position);
 
@@ -33,6 +37,36 @@ function pumpRecording(data: Uint8Array): GameInputEvent[] {
         });
     }
     return events;
+}
+
+function getVersion(versionStr: string): [number, number, number] {
+    const versionMatch = versionStr.match(/\d*\.\d*\.\d*/);
+    if (!versionMatch) {
+        return [0, 0, 0];
+    }
+
+    const versionSplit = versionMatch[0].split(".");
+    return [
+        parseInt(versionSplit[0] ?? "0"),
+        parseInt(versionSplit[1] ?? "0"),
+        parseInt(versionSplit[2] ?? "0")
+    ];
+}
+
+function checkMinVersion(min: [number, number, number], version: [number, number, number]): boolean {
+    if (min[0] > version[0]) {
+        return false;
+    } else if (min[0] < version[0]) {
+        return true;
+    }
+
+    if (min[1] > version[1]) {
+        return false;
+    } else if (min[1] < version[1]) {
+        return true;
+    }
+
+    return min[2] <= version[2];
 }
 
 export async function parseReplayFromBuffer(replayBuf: Buffer): Promise<GameReplayData> {
@@ -46,7 +80,14 @@ export async function parseReplayFromBuffer(replayBuf: Buffer): Promise<GameRepl
     const metadataStr = Buffer.from(metadata).toString();
     const replayData = JSON.parse(metadataStr) as Partial<GameReplayData>;
     
-    replayData.inputs = pumpRecording(Buffer.from(data));
+    // Replay versions above v0.17.21 use absolute timing
+    const version = getVersion(replayData.version ?? "0.0.0");
+    const minVersion = [0, 17, 21] as [number, number, number];
+    const useAbsoluteTiming = checkMinVersion(minVersion, version);
+
+    const buf = Buffer.from(data);
+
+    replayData.inputs = pumpRecording(new Uint8Array(buf), useAbsoluteTiming);
 
     return replayData as GameReplayData;
 }
