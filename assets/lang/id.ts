42572e82e671d09e1254962adda3ecddfe023ce1
NOT_A_ROBOT <not_a_normal_robot@protonmail.com>
2026-03-28 01:54:30 +0700

    #98 step 1 indonesian

--- assets/lang/id.ts
+++ assets/lang/id.ts
@@ -20,7 +20,16 @@ export default {
     tabTitle: "Techmino Hub - Beranda",
     title: "Selamat datang di Techmino Hub!",
     article: `md_Dibuat ulang di Nuxt.js, Techmino Hub adalah website Techmino yang dibuat oleh komunitas Techmino.  
-    Website ini sedang dalam fase mula, fitur-fitur baru akan muncul pada masa depan.`
+    Website ini sedang dalam fase mula, fitur-fitur baru akan muncul pada masa depan.
+    
+    # Segera: Papan Peringkat Akan Ditutup
+    
+    Karena kombinasi sumber daya pengembang yang terbatas dan perubahan besar Supabase yang akan datang, **papan peringkat tidak akan lagi dapat diakses melalui situs setelah 8 April**.  
+    Demi melestarikan riwayat Techmino, akan disediakan pengunduhan basis data publik setelah tanggal tersebut.
+    
+    Halaman statis, seperti FAQ dan peta, tidak terpengaruh.
+    
+    Lihat [masalah #98 di GitHub](https://github.com/techmino-hub/techmino-hub-source-code/issues/98) untuk detail lebih lanjut. Seperti biasa, kami menerima kontributor/pengembang baru jika Anda merasa mampu untuk tugas ini.`
   },
 
   lang: {
